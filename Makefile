1ff9ed1494e51621526172a71e3663e404934f0f
Filippo Valsorda <hi@filippo.io>
2018-01-07 15:38:15 +0100

    Workaround relic-toolkit/relic#61

--- Makefile
+++ Makefile
@@ -1,12 +1,9 @@
 .PHONY: relic
 relic:
 	rm -rf relic/build && mkdir relic/build
-	# https://github.com/relic-toolkit/relic/issues/61
 	cd relic/build && cmake -DALLOC=DYNAMIC -DFP_PRIME=381 \
 		-DSHLIB=off -DSTLIB=on -DRAND=UDEV -DTESTS=1 -DBENCH=0 \
-		-DCOMP="-O3 -funroll-loops -Wno-unused-function" \
-		-DFP_METHD="BASIC;COMBA;COMBA;BASIC;BASIC;SLIDE" \
-		../relic
+		-DCOMP="-O3 -funroll-loops -Wno-unused-function" ../relic
 	make -C relic/build
 	make -C relic/build test
 
--- relic/relic.c
+++ relic/relic.c
@@ -6,15 +6,23 @@
 // https://github.com/relic-toolkit/relic/issues/60
 
 int _y_is_higher(const ep_t ep) {
+    uint8_t a[FP_BYTES], b[FP_BYTES];
     fp_t other;
+
+    fp_write_bin(a, FP_BYTES, ep->y);
+
     fp_new(other);
     fp_copy(other, ep->y);
     fp_neg(other, other);
-    int res = fp_cmp(ep->y, other);
+    fp_write_bin(b, FP_BYTES, other);
     fp_free(other);
-    if (res == CMP_GT) {
-        return 1;
-    } else {
-        return 0;
+
+    for (int i = 0; i < FP_BYTES; i++) {
+        if (a[i] > b[i]) {
+            return 1;
+        } else if (a[i] < b[i]) {
+            return 0;
+        }
     }
+    return 0;
 }
